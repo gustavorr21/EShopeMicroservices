708e1ac52b9b6a2c79a5b5b76ce7419bd1693249
Gustavo Ricardo <gustavo_rricardo@hotmail.com>
2025-08-11 13:45:08 -0300

    criacao do crud itens e do docker

---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,16 @@
+﻿global using Basket.API.Models;
+global using BuildingBlocks.CQRS;
+global using Carter;
+global using MediatR;
+global using Mapster;
+global using FluentValidation;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions;
+global using Marten;
+global using BuildingBlocks.Exceptions.Handler;
+global using Basket.API.Exceptions;
+global using Discount.Grpc;
+global using HealthChecks.UI.Client;
+global using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+global using BuildingBlocks.Messaging.MassTransit;
+global using Basket.API.Data;
